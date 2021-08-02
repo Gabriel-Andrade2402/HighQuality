67db7f5ca0e1159f7a0ed271eee91a3b3c8b3e4e
Gabriel de andrade silva <gabriel.andrade2402@gmail.com>
2021-08-02 19:54:02 -0300

    inicio da criação dos databases para utilizar como portifolio

--- scripts/javascriptGaleriaDesign.js
+++ scripts/javascriptGaleriaDesign.js
@@ -1,4 +1,10 @@
-var eventoTexto;
+//O EVENTOTEXTO não tem importancia de ser um só pois ele é executado apenas uma vez por metodo
+var eventoTexto=null;
+var click_chamativos=null;
+var click_informativos=null;
+var click_cardapios=null;
+var click_logomarcas=null;
+
 
 //EVENTOS "CHAMATIVOS"
 function CriandoTextoChamativos(){
@@ -7,45 +13,46 @@ function CriandoTextoChamativos(){
 	var div=document.getElementById('blocoTituloChamativo');
 	var span=div.children[0];
 	icone.style.opacity="0";
-	if(eventoTexto==null){
-	article.style.background="linear-gradient(to left top,#e4e4e4,#20B2AA)";
-	eventoTexto=window.setInterval(function(){
-		var arrayTexto=span.innerText.split("");
-		span.style.fontSize ="35pt";
-		span.style.position="relative";
-		span.style.left="25%";
-		span.style.top="80%";
-		if(arrayTexto.length==0){
-			span.innerText="C";
-		}
-		if(arrayTexto.length==1){
-			span.innerText+="H";
-		}if(arrayTexto.length==2){
-			span.innerText+="A";
-		}
-		if(arrayTexto.length==3){
-			span.innerText+="M";
-		}
-		if(arrayTexto.length==4){
-			span.innerText+="A";
-		}
-		if(arrayTexto.length==5){
-			span.innerText+="T";	
-		}
-		if(arrayTexto.length==6){
-			span.innerText+="I";
-		}
-		if(arrayTexto.length==7){
-			span.innerText+="V";
-		}
-		if(arrayTexto.length==8){
-			span.innerText+="O";
-		}
-		if(arrayTexto.length==9){
-			span.innerText+="S";
-			cancelandoTexto();
-		}
-	}, 120);
+	if(eventoTexto===null && click_chamativos=== null){
+		eventoTexto=window.setInterval(function(){
+			article.style.background="linear-gradient(to left top,#e4e4e4,#20B2AA)";
+			article.style.border="0.5px solid black";
+			var arrayTexto=span.innerText.split("");
+			span.style.fontSize ="35pt";
+			span.style.position="relative";
+			span.style.left="25%";
+			span.style.top="80%";
+			if(arrayTexto.length==0){
+				span.innerText="C";
+			}
+			if(arrayTexto.length==1){
+				span.innerText+="H";
+			}if(arrayTexto.length==2){
+				span.innerText+="A";
+			}
+			if(arrayTexto.length==3){
+				span.innerText+="M";
+			}
+			if(arrayTexto.length==4){
+				span.innerText+="A";
+			}
+			if(arrayTexto.length==5){
+				span.innerText+="T";	
+			}
+			if(arrayTexto.length==6){
+				span.innerText+="I";
+			}
+			if(arrayTexto.length==7){
+				span.innerText+="V";
+			}
+			if(arrayTexto.length==8){
+				span.innerText+="O";
+			}
+			if(arrayTexto.length==9){
+				span.innerText+="S";
+				cancelandoTexto();
+			}
+		}, 120);
 	}
 }
 
@@ -60,6 +67,7 @@ function cancelarChamativos(){
 	span.style.left="30%";
 	span.style.marginTop="0%";
 	article.style.background="linear-gradient(to left top,#e4e4e4,#1E90FF)";
+	article.style.border="0.5px solid #00BFFF";
 }
 //-------------FIM EVENTOS "CHAMATIVOS"
 
@@ -68,12 +76,15 @@ function cancelarChamativos(){
 
 //EVENTOS "CARDAPIOS"
 function CriandoTextoCardapios(){
+	var article=document.getElementById('articleCardapios');
 	var icone=document.getElementById('iconeCardapios');
 	var div=document.getElementById('blocoTituloCardapios');
 	var span=div.children[0];
 	icone.style.opacity="0";
-	if(eventoTexto==null){
+	if(eventoTexto==null && click_cardapios=== null){
 	eventoTexto=window.setInterval(function(){
+		article.style.background="linear-gradient(to left top,#20B2AA,#e4e4e4)";
+		article.style.border="0.5px solid black";
 		var arrayTexto=span.innerText.split("");
 		span.style.fontSize ="35pt";
 		span.style.position="relative";
@@ -112,6 +123,7 @@ function CriandoTextoCardapios(){
 
 function cancelarCardapios(){
 	cancelandoTexto();
+	var article=document.getElementById('articleCardapios');
 	var div=document.getElementById('blocoTituloCardapios');
 	var icone=document.getElementById('iconeCardapios');
 	var span=div.children[0];
@@ -119,18 +131,23 @@ function cancelarCardapios(){
 	span.innerText="";
 	span.style.left="30%";
 	span.style.marginTop="0%";
+	article.style.background="linear-gradient(to left top,#1E90FF,#e4e4e4)";
+	article.style.border="0.5px solid #00BFFF";
 }
 //-------------FIM EVENTOS "CARDAPIOS"
 
 
 //EVENTOS "INFORMATIVOS"
 function CriandoTextoInformativos(){
+	var article=document.getElementById('articleInformativos');
 	var icone=document.getElementById('iconeInformativos');
 	var div=document.getElementById('blocoTituloInformativos');
 	var span=div.children[0];
 	icone.style.opacity="0";
-	if(eventoTexto==null){
+	if(eventoTexto==null && click_informativos=== null){
 	eventoTexto=window.setInterval(function(){
+		article.style.background="linear-gradient(to left bottom,#20B2AA,#e4e4e4)";
+		article.style.border="0.5px solid black";
 		var arrayTexto=span.innerText.split("");
 		span.style.fontSize ="30pt";
 		span.style.position="relative";
@@ -178,6 +195,7 @@ function CriandoTextoInformativos(){
 
 function cancelarInformativos(){
 	cancelandoTexto();
+	var article=document.getElementById('articleInformativos');
 	var div=document.getElementById('blocoTituloInformativos');
 	var icone=document.getElementById('iconeInformativos');
 	var span=div.children[0];
@@ -185,17 +203,22 @@ function cancelarInformativos(){
 	span.innerText="";
 	span.style.left="30%";
 	span.style.marginTop="0%";
+	article.style.background="linear-gradient(to left bottom,#1E90FF,#e4e4e4)";
+	article.style.border="0.5px solid #00BFFF";
 }
 //-------------FIM EVENTOS "INFORMATIVOS"
 
 
 //EVENTOS "LOGOMARCAS"
 function CriandoTextoLogomarcas(){
+	var article=document.getElementById('articleLogomarcas');
 	var icone=document.getElementById('iconeLogomarcas');
 	var div=document.getElementById('blocoTituloLogomarcas');
 	var span=div.children[0];
 	icone.style.opacity="0";
-	if(eventoTexto==null){
+	if(eventoTexto==null && click_logomarcas=== null){
+	article.style.background="linear-gradient(to left,#20B2AA,#e4e4e4)";
+	article.style.border="0.5px solid black";
 	eventoTexto=window.setInterval(function(){
 		var arrayTexto=span.innerText.split("");
 		span.style.fontSize ="30pt";
@@ -238,6 +261,7 @@ function CriandoTextoLogomarcas(){
 
 function cancelarLogomarcas(){
 	cancelandoTexto();
+	var article=document.getElementById('articleLogomarcas');
 	var div=document.getElementById('blocoTituloLogomarcas');
 	var icone=document.getElementById('iconeLogomarcas');
 	var span=div.children[0];
@@ -245,6 +269,8 @@ function cancelarLogomarcas(){
 	span.innerText="";
 	span.style.left="30%";
 	span.style.marginTop="0%";
+	article.style.background="linear-gradient(to left,#1E90FF,#e4e4e4)";
+	article.style.border="0.5px solid #00BFFF";
 }
 //-------------FIM EVENTOS "LOGOMARCAS"
 
@@ -254,7 +280,8 @@ function cancelandoTexto(){
 	window.clearInterval(eventoTexto);
 	eventoTexto=null;
 }
-function limparConteudo(icone,div,article,span,fundoRetrair,botaoRetrair,blocoComImagens,valorScrollY){
+function limparConteudo(icone,div,article,span,fundoRetrair,botaoRetrair,blocoComImagens,
+	valorScrollY,conteudo_clicado){
 	setTimeout(function(){
 		article.style.width="400px";
 		article.style.height="400px";
@@ -264,6 +291,22 @@ function limparConteudo(icone,div,article,span,fundoRetrair,botaoRetrair,blocoCo
 		article.removeChild(botaoRetrair);
 		article.removeChild(blocoComImagens);
 		document.documentElement.scrollTop = valorScrollY;
+		if(conteudo_clicado==="click_chamativos"){
+			click_chamativos=null;
+			cancelarChamativos();
+		}
+		if(conteudo_clicado==="click_cardapios"){
+			click_cardapios=null;
+			cancelarCardapios();
+		}
+		if(conteudo_clicado==="click_informativos"){
+			click_informativos=null;
+			cancelarInformativos();
+		}
+		if(conteudo_clicado==="click_logomarcas"){
+			click_logomarcas=null;
+			cancelarLogomarcas();
+		}
 	},100);
 }
 function iniciaImagensEAdiciona(blocoComImagens,arrayDeImagens,width,height){
@@ -277,7 +320,8 @@ function iniciaImagensEAdiciona(blocoComImagens,arrayDeImagens,width,height){
 		blocoComImagens.appendChild(imagem);
 	});
 }
-function iniciaBotaoRetrair(icone,div,article,span,fundoRetrair,botaoRetrair,blocoComImagens,valorScrollY){
+function iniciaBotaoRetrair(icone,div,article,span,fundoRetrair,botaoRetrair,blocoComImagens,
+	valorScrollY,conteudo_clicado){
 	fundoRetrair.classList="formatacaoFundoRetrair";
 	botaoRetrair.classList="formatacaoBotaoRetrair";
 	fundoRetrair.onmouseover = function(){
@@ -297,16 +341,19 @@ function iniciaBotaoRetrair(icone,div,article,span,fundoRetrair,botaoRetrair,blo
 			botaoRetrair.style.opacity="0.5";
 		};
 		fundoRetrair.addEventListener('click',function(){
-			limparConteudo(icone,div,article,span,fundoRetrair,botaoRetrair,blocoComImagens,valorScrollY);
+			limparConteudo(icone,div,article,span,fundoRetrair,botaoRetrair,blocoComImagens,
+				valorScrollY,conteudo_clicado);
 		});
 		botaoRetrair.addEventListener('click',function(){
-			limparConteudo(icone,div,article,span,fundoRetrair,botaoRetrair,blocoComImagens,valorScrollY);
+			limparConteudo(icone,div,article,span,fundoRetrair,botaoRetrair,blocoComImagens,
+				valorScrollY,conteudo_clicado);
 		});
 }
 
 
 //Eventos de cliques na galeria
 function cliqueChamativos(){
+	click_chamativos="1";
 	var icone=document.getElementById('iconeChamativo');
 	var div=document.getElementById('blocoTituloChamativo');
 	var article=document.getElementById('articleChamativos');
@@ -339,7 +386,7 @@ function cliqueChamativos(){
 		iniciaImagensEAdiciona(blocoComImagens,arrayDeImagens,"980px","300px");
 		//Inicia botao de retração de conteudo
 		botaoRetrair.src="../imagens/iconeEsquerda.png";
-		iniciaBotaoRetrair(icone,div,article,span,fundoRetrair,botaoRetrair,blocoComImagens);
+		iniciaBotaoRetrair(icone,div,article,span,fundoRetrair,botaoRetrair,blocoComImagens,0,"click_chamativos");
 		//Fim da criação do botão de retração
 		article.style.width="1000px";
 		article.style.height="500px";
@@ -353,6 +400,7 @@ function cliqueChamativos(){
 	}
 }
 function cliqueCardapios(){
+	click_cardapios="1";
 	var icone=document.getElementById('iconeCardapios');
 	var div=document.getElementById('blocoTituloCardapios');
 	var article=document.getElementById('articleCardapios');
@@ -385,7 +433,7 @@ function cliqueCardapios(){
 		iniciaImagensEAdiciona(blocoComImagens,arrayDeImagens,"980px","350px");
 		//Inicia botao de retração de conteudo
 		botaoRetrair.src="../imagens/iconeEsquerda.png";
-		iniciaBotaoRetrair(icone,div,article,span,fundoRetrair,botaoRetrair,blocoComImagens,0);
+		iniciaBotaoRetrair(icone,div,article,span,fundoRetrair,botaoRetrair,blocoComImagens,0,"click_cardapios");
 		//Fim da criação do botão de retração
 		article.style.width="1000px";
 		article.style.height="500px";
@@ -402,6 +450,7 @@ function cliqueCardapios(){
 }
 
 function cliqueInformativos(){
+	click_informativos="1";
 	var icone=document.getElementById('iconeInformativos');
 	var div=document.getElementById('blocoTituloInformativos');
 	var article=document.getElementById('articleInformativos');
@@ -422,7 +471,7 @@ function cliqueInformativos(){
 		iniciaImagensEAdiciona(blocoComImagens,arrayDeImagens,"980px","300px");
 		//Inicia botao de retração de conteudo
 		botaoRetrair.src="../imagens/iconeEsquerda.png";
-		iniciaBotaoRetrair(icone,div,article,span,fundoRetrair,botaoRetrair,blocoComImagens,600);
+		iniciaBotaoRetrair(icone,div,article,span,fundoRetrair,botaoRetrair,blocoComImagens,600,"click_informativos");
 		//Fim da criação do botão de retração
 		article.style.width="1000px";
 		article.style.height="500px";
@@ -438,6 +487,7 @@ function cliqueInformativos(){
 	}
 }
 function cliqueLogomarcas(){
+	click_logomarcas="1";
 	var icone=document.getElementById('iconeLogomarcas');
 	var div=document.getElementById('blocoTituloLogomarcas');
 	var article=document.getElementById('articleLogomarcas');
@@ -455,7 +505,7 @@ function cliqueLogomarcas(){
 		iniciaImagensEAdiciona(blocoComImagens,arrayDeImagens,"980px","300px");
 		//Inicia botao de retração de conteudo
 		botaoRetrair.src="../imagens/iconeEsquerda.png";
-		iniciaBotaoRetrair(icone,div,article,span,fundoRetrair,botaoRetrair,blocoComImagens,500);
+		iniciaBotaoRetrair(icone,div,article,span,fundoRetrair,botaoRetrair,blocoComImagens,500,"click_logomarcas");
 		//Fim da criação do botão de retração
 		article.style.width="1000px";
 		article.style.height="500px";
