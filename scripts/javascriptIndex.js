93d6eac4f1e63bcc58cd3948ac07aa8d178bb4e1
Gabriel de andrade silva <gabriel.andrade2402@gmail.com>
2021-08-09 15:43:01 -0300

    fim da recriação do template Mobile index

--- scripts/javascriptIndex.js
+++ scripts/javascriptIndex.js
@@ -3,6 +3,9 @@ function ScrollManipulation(){
 		var menu=document.getElementById('menu');
 		menu.style.position="absolute";
 		if (window.scrollY >300){
+			let article=document.getElementById('articlePlanilhas');
+    		menu.style.position="fixed";
+    		article.style.left="0px";
 			let backgrounArticle=document.getElementById('imageBackgroundArticleDatabase');
     		let icone=document.getElementById('iconeArticle1');
     		let tituloArticle=document.getElementById('tituloArticle1');
@@ -30,10 +33,21 @@ function ScrollManipulation(){
     		backgrounArticle.style.opacity="1";
     		document.body.style.background="#DC143C";
     	}
-    	if(window.scrollY>500){
-    		let article=document.getElementById('articlePlanilhas');
-    		menu.style.position="fixed";
-    		article.style.left="0px";
+    	if(window.scrollY>1400){
+    		let backgrounArticle=document.getElementById('imageBackgroundArticleMobile');
+    		let icone=document.getElementById('imagemArticle3');
+    		let tituloArticle=document.getElementById('tituloArticle3');
+    		let conteudoArticle=document.getElementById('conteudoArticle3');
+    		tituloArticle.style.left="5%";
+    		tituloArticle.style.opacity="1";
+    		conteudoArticle.style.left="50%";
+    		conteudoArticle.style.opacity="1";
+    		icone.style.left="0%";
+    		icone.style.opacity="1";
+    		backgrounArticle.style.opacity="1";
+    		backgrounArticle.style.position="relative";
+    		backgrounArticle.style.top="-12%";
+    		document.body.style.background="#59f240";
     	}
     	if(window.scrollY>800){
     		let article=document.getElementById('articleMobile');
