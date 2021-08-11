9bed3f618995d5e579ebf891eb3f875ca73602e2
Gabriel de andrade silva <gabriel.andrade2402@gmail.com>
2021-08-11 16:15:17 -0300

    fim da criação de todos metodos faltando apenas o download sql

--- scripts/javascriptGaleriaDatabase.js
+++ scripts/javascriptGaleriaDatabase.js
@@ -347,9 +347,354 @@ function clickDatabasesPorAPI(){
 		},500);
 	}
 }
-function clickDatabasesProcessadoras(){}
-function clickDatabasesSegurança(){}
-function clickDatabasesTransacionais(){}
+function clickDatabasesProcessadoras(){
+	let article=document.getElementById('articleDatabaseProcessadoras');
+	article.onclick=null;
+	let textDownload=document.createElement('a');
+	let textoExplicativo=document.createElement('span');
+	let video=document.createElement('video');
+	let imagens=document.createElement('img');
+	let blockImagens=document.createElement('div');
+	let blockFechar=document.createElement('div');
+	let imgFechar=document.createElement('img');
+	let inicioTitulo=document.getElementById('textTituloArticle4');
+	let fimTitulo=document.getElementById('blocoTextoSimples4');
+	if(article.style.width!="90%"){
+		article.style.width="90%";
+		article.style.height="450px";
+		article.style.marginLeft="0%";
+		setTimeout(function(){
+			article.classList.remove('articleDatabases');
+			article.classList.add('articleDatabaseOpen');
+			//Formatar e manipular titulo ===================
+			inicioTitulo.classList.add('inicioTitulo');
+			inicioTitulo.style.marginLeft="0%";
+			fimTitulo.classList.add('fimTitulo');
+			//Fim -------------------------------------------
+			//Formatar texto Download =======================
+			let iconeBaixar=document.createElement('img');
+			iconeBaixar.src="../imagens/iconeBaixar.png";
+			iconeBaixar.classList.add('iconeBaixar');
+			textDownload.classList.add('textDownload');
+			textDownload.href="https://www.facebook.com"
+			textDownload.target="blank";
+			textDownload.appendChild(iconeBaixar);
+			//Fim -------------------------------------------
+			//Formatar texto conteudo =======================
+			textoExplicativo.textContent="Este banco de dados é simples e possui apenas uma tabela"+
+			" com os informações";
+			textoExplicativo.classList.add('textoExplicativo');
+			//Fim -------------------------------------------
+			//Formatar video  ===============================
+			video.id="videoDatabaseBasicos";
+			video.src="../videos/VideoBancoDeDados4.mp4";
+			video.classList.add('video');
+			video.controls = true;
+			//Fim -------------------------------------------
+			//Formatar botao fechar imagens  =============================
+			blockFechar.classList.add('blockFechar');
+			blockFechar.onmouseover=function(){
+				blockFechar.style.opacity="0.7";
+				imgFechar.style.opacity="0.7";
+			};
+			blockFechar.onmouseleave=function(){
+				blockFechar.style.opacity="0.2";
+				imgFechar.style.opacity="0.2";
+			};
+			blockFechar.onclick=function(){
+				article.removeChild(textDownload);
+				article.removeChild(textoExplicativo);
+				article.removeChild(video);
+				article.removeChild(blockImagens);
+				article.removeChild(imgFechar);
+				article.removeChild(blockFechar);
+				article.style.width="60%";
+				article.style.height="60px";
+				article.style.marginLeft="13%";
+				article.classList.remove('articleDatabaseOpen');
+				article.classList.add('articleDatabases');
+				inicioTitulo.style.marginLeft="30%";
+				setTimeout(function (){
+				 article.onclick=clickDatabasesBasico;
+				},200);
+			}
+			imgFechar.src="../imagens/iconeSubir.png";
+			imgFechar.classList.add('imgFechar');
+			imgFechar.onmouseover=function(){
+				blockFechar.style.opacity="0.7";
+				imgFechar.style.opacity="0.7";
+			};
+			imgFechar.onmouseleave=function(){
+				blockFechar.style.opacity="0.2";
+				imgFechar.style.opacity="0.2";
+			};
+			imgFechar.onclick=function(){
+				article.removeChild(textDownload);
+				article.removeChild(textoExplicativo);
+				article.removeChild(video);
+				article.removeChild(blockImagens);
+				article.removeChild(imgFechar);
+				article.removeChild(blockFechar);
+				article.style.width="60%";
+				article.style.height="60px";
+				article.style.marginLeft="13%";
+				article.classList.remove('articleDatabaseOpen');
+				article.classList.add('articleDatabases');
+				inicioTitulo.style.marginLeft="30%";
+				setTimeout(function (){
+				 article.onclick=clickDatabasesBasico;
+				},200);
+			};
+			//Fim -------------------------------------------
+			//Formatar slide imagens  =============================
+			blockImagens.classList.add('blockImagens');
+			imagens.id="databaseProcedimentos-1";
+			imagens.src="../imagens/databases/procedimento/databaseProcedimentos-1.png";
+			imagens.classList.add('imagens');
+			blockImagens.appendChild(imagens);
+			iniciarManipulacaoSlide(blockImagens,imagens,'procedimento',3);
+			//Fim -------------------------------------------
+			article.appendChild(textDownload);
+			article.appendChild(textoExplicativo);
+			article.appendChild(video);
+			article.appendChild(blockFechar);
+			article.appendChild(imgFechar);
+			article.appendChild(blockImagens);
+		},500);
+	}
+}
+function clickDatabasesSegurança(){
+	let article=document.getElementById('articleDatabaseSegurança');
+	article.onclick=null;
+	let textDownload=document.createElement('a');
+	let textoExplicativo=document.createElement('span');
+	let video=document.createElement('video');
+	let imagens=document.createElement('img');
+	let blockImagens=document.createElement('div');
+	let blockFechar=document.createElement('div');
+	let imgFechar=document.createElement('img');
+	let inicioTitulo=document.getElementById('textTituloArticle5');
+	let fimTitulo=document.getElementById('blocoTextoSimples5');
+	if(article.style.width!="90%"){
+		article.style.width="90%";
+		article.style.height="450px";
+		article.style.marginLeft="0%";
+		setTimeout(function(){
+			article.classList.remove('articleDatabases');
+			article.classList.add('articleDatabaseOpen');
+			//Formatar e manipular titulo ===================
+			inicioTitulo.classList.add('inicioTitulo');
+			inicioTitulo.style.marginLeft="0%";
+			fimTitulo.classList.add('fimTitulo');
+			//Fim -------------------------------------------
+			//Formatar texto Download =======================
+			let iconeBaixar=document.createElement('img');
+			iconeBaixar.src="../imagens/iconeBaixar.png";
+			iconeBaixar.classList.add('iconeBaixar');
+			textDownload.classList.add('textDownload');
+			textDownload.href="https://www.facebook.com"
+			textDownload.target="blank";
+			textDownload.appendChild(iconeBaixar);
+			//Fim -------------------------------------------
+			//Formatar texto conteudo =======================
+			textoExplicativo.textContent="Este banco de dados é simples e possui apenas uma tabela"+
+			" com os informações";
+			textoExplicativo.classList.add('textoExplicativo');
+			//Fim -------------------------------------------
+			//Formatar video  ===============================
+			video.id="videoDatabaseBasicos";
+			video.src="../videos/VideoBancoDeDados5.mp4";
+			video.classList.add('video');
+			video.controls = true;
+			//Fim -------------------------------------------
+			//Formatar botao fechar imagens  =============================
+			blockFechar.classList.add('blockFechar');
+			blockFechar.onmouseover=function(){
+				blockFechar.style.opacity="0.7";
+				imgFechar.style.opacity="0.7";
+			};
+			blockFechar.onmouseleave=function(){
+				blockFechar.style.opacity="0.2";
+				imgFechar.style.opacity="0.2";
+			};
+			blockFechar.onclick=function(){
+				article.removeChild(textDownload);
+				article.removeChild(textoExplicativo);
+				article.removeChild(video);
+				article.removeChild(blockImagens);
+				article.removeChild(imgFechar);
+				article.removeChild(blockFechar);
+				article.style.width="60%";
+				article.style.height="60px";
+				article.style.marginLeft="13%";
+				article.classList.remove('articleDatabaseOpen');
+				article.classList.add('articleDatabases');
+				inicioTitulo.style.marginLeft="30%";
+				setTimeout(function (){
+				 article.onclick=clickDatabasesBasico;
+				},200);
+			}
+			imgFechar.src="../imagens/iconeSubir.png";
+			imgFechar.classList.add('imgFechar');
+			imgFechar.onmouseover=function(){
+				blockFechar.style.opacity="0.7";
+				imgFechar.style.opacity="0.7";
+			};
+			imgFechar.onmouseleave=function(){
+				blockFechar.style.opacity="0.2";
+				imgFechar.style.opacity="0.2";
+			};
+			imgFechar.onclick=function(){
+				article.removeChild(textDownload);
+				article.removeChild(textoExplicativo);
+				article.removeChild(video);
+				article.removeChild(blockImagens);
+				article.removeChild(imgFechar);
+				article.removeChild(blockFechar);
+				article.style.width="60%";
+				article.style.height="60px";
+				article.style.marginLeft="13%";
+				article.classList.remove('articleDatabaseOpen');
+				article.classList.add('articleDatabases');
+				inicioTitulo.style.marginLeft="30%";
+				setTimeout(function (){
+				 article.onclick=clickDatabasesBasico;
+				},200);
+			};
+			//Fim -------------------------------------------
+			//Formatar slide imagens  =============================
+			blockImagens.classList.add('blockImagens');
+			imagens.id="databaseSeguranca-1";
+			imagens.src="../imagens/databases/seguranca/databaseSeguranca-1.png";
+			imagens.classList.add('imagens');
+			blockImagens.appendChild(imagens);
+			iniciarManipulacaoSlide(blockImagens,imagens,'seguranca',5);
+			//Fim -------------------------------------------
+			article.appendChild(textDownload);
+			article.appendChild(textoExplicativo);
+			article.appendChild(video);
+			article.appendChild(blockFechar);
+			article.appendChild(imgFechar);
+			article.appendChild(blockImagens);
+		},500);
+	}
+}
+function clickDatabasesTransacionais(){
+	let article=document.getElementById('articleDatabaseTransacionais');
+	article.onclick=null;
+	let textDownload=document.createElement('a');
+	let textoExplicativo=document.createElement('span');
+	let video=document.createElement('video');
+	let imagens=document.createElement('img');
+	let blockImagens=document.createElement('div');
+	let blockFechar=document.createElement('div');
+	let imgFechar=document.createElement('img');
+	let inicioTitulo=document.getElementById('textTituloArticle6');
+	let fimTitulo=document.getElementById('blocoTextoSimples6');
+	if(article.style.width!="90%"){
+		article.style.width="90%";
+		article.style.height="450px";
+		article.style.marginLeft="0%";
+		setTimeout(function(){
+			article.classList.remove('articleDatabases');
+			article.classList.add('articleDatabaseOpen');
+			//Formatar e manipular titulo ===================
+			inicioTitulo.classList.add('inicioTitulo');
+			inicioTitulo.style.marginLeft="0%";
+			fimTitulo.classList.add('fimTitulo');
+			//Fim -------------------------------------------
+			//Formatar texto Download =======================
+			let iconeBaixar=document.createElement('img');
+			iconeBaixar.src="../imagens/iconeBaixar.png";
+			iconeBaixar.classList.add('iconeBaixar');
+			textDownload.classList.add('textDownload');
+			textDownload.href="https://www.facebook.com"
+			textDownload.target="blank";
+			textDownload.appendChild(iconeBaixar);
+			//Fim -------------------------------------------
+			//Formatar texto conteudo =======================
+			textoExplicativo.textContent="Este banco de dados é simples e possui apenas uma tabela"+
+			" com os informações";
+			textoExplicativo.classList.add('textoExplicativo');
+			//Fim -------------------------------------------
+			//Formatar video  ===============================
+			video.id="videoDatabaseBasicos";
+			video.src="../videos/VideoBancoDeDados6.mp4";
+			video.classList.add('video');
+			video.controls = true;
+			//Fim -------------------------------------------
+			//Formatar botao fechar imagens  =============================
+			blockFechar.classList.add('blockFechar');
+			blockFechar.onmouseover=function(){
+				blockFechar.style.opacity="0.7";
+				imgFechar.style.opacity="0.7";
+			};
+			blockFechar.onmouseleave=function(){
+				blockFechar.style.opacity="0.2";
+				imgFechar.style.opacity="0.2";
+			};
+			blockFechar.onclick=function(){
+				article.removeChild(textDownload);
+				article.removeChild(textoExplicativo);
+				article.removeChild(video);
+				article.removeChild(blockImagens);
+				article.removeChild(imgFechar);
+				article.removeChild(blockFechar);
+				article.style.width="60%";
+				article.style.height="60px";
+				article.style.marginLeft="13%";
+				article.classList.remove('articleDatabaseOpen');
+				article.classList.add('articleDatabases');
+				inicioTitulo.style.marginLeft="30%";
+				setTimeout(function (){
+				 article.onclick=clickDatabasesBasico;
+				},200);
+			}
+			imgFechar.src="../imagens/iconeSubir.png";
+			imgFechar.classList.add('imgFechar');
+			imgFechar.onmouseover=function(){
+				blockFechar.style.opacity="0.7";
+				imgFechar.style.opacity="0.7";
+			};
+			imgFechar.onmouseleave=function(){
+				blockFechar.style.opacity="0.2";
+				imgFechar.style.opacity="0.2";
+			};
+			imgFechar.onclick=function(){
+				article.removeChild(textDownload);
+				article.removeChild(textoExplicativo);
+				article.removeChild(video);
+				article.removeChild(blockImagens);
+				article.removeChild(imgFechar);
+				article.removeChild(blockFechar);
+				article.style.width="60%";
+				article.style.height="60px";
+				article.style.marginLeft="13%";
+				article.classList.remove('articleDatabaseOpen');
+				article.classList.add('articleDatabases');
+				inicioTitulo.style.marginLeft="30%";
+				setTimeout(function (){
+				 article.onclick=clickDatabasesBasico;
+				},200);
+			};
+			//Fim -------------------------------------------
+			//Formatar slide imagens  =============================
+			blockImagens.classList.add('blockImagens');
+			imagens.id="databaseTransacoes-1";
+			imagens.src="../imagens/databases/transacionais/databaseTransacoes-1.png";
+			imagens.classList.add('imagens');
+			blockImagens.appendChild(imagens);
+			iniciarManipulacaoSlide(blockImagens,imagens,'transacionais',5);
+			//Fim -------------------------------------------
+			article.appendChild(textDownload);
+			article.appendChild(textoExplicativo);
+			article.appendChild(video);
+			article.appendChild(blockFechar);
+			article.appendChild(imgFechar);
+			article.appendChild(blockImagens);
+		},500);
+	}
+}
 
 //Métodos auxiliar para passar e voltar  imagens manipulando a string com id "imagensCakesTreats-1"
 //cria e inicia o slide de imagens
